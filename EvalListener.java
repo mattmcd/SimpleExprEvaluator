f21c847a57d90ce015da7a4a2356cbee0dff8d73
mattmcd <matt@matt-mcdonnell.com>
2014-09-21 18:51:34 +0100

    Changed debug println statements to log statements.

--- EvalListener.java
+++ EvalListener.java
@@ -1,23 +1,36 @@
 import java.util.Stack;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EvalListener extends ExprBaseListener {
   private Stack<Integer> theStack = new Stack<Integer>(); 
   private Map<String,Integer> map = new HashMap<String,Integer>();
+  private final static Logger LOGGER = Logger.getLogger(
+    EvalListener.class.getName());
+
+  public EvalListener( ) {
+    LOGGER.setLevel(Level.SEVERE);
+  }
+
+  public void setVerbose( boolean isVerbose ) {
+    if ( isVerbose ) LOGGER.setLevel(Level.INFO);
+    else LOGGER.setLevel(Level.SEVERE);
+  }
 
 	@Override public void exitId(ExprParser.IdContext ctx) { 
-    System.out.println( "Exiting Id" );
-    System.out.println( "  Pushing value of " + 
+    LOGGER.info( "Exiting Id" );
+    LOGGER.info( "  Pushing value of " + 
       ctx.ID().getText() + " to stack" );
     theStack.push( map.get( ctx.ID().getText()));
   }
 
 	@Override public void enterAssign(ExprParser.AssignContext ctx) { 
-    System.out.println( "Entering Assign" );
+    LOGGER.info( "Entering Assign" );
   }
 	@Override public void exitAssign(ExprParser.AssignContext ctx) { 
-    System.out.println( "Exiting Assign" );
+    LOGGER.info( "Exiting Assign" );
     map.put( ctx.ID().getText(), theStack.pop());
   }
 
@@ -28,13 +41,13 @@ public class EvalListener extends ExprBaseListener {
 
 	@Override 
   public void exitInt(ExprParser.IntContext ctx) { 
-    System.out.println( "Exiting Int" );
+    LOGGER.info( "Exiting Int" );
     theStack.push( Integer.parseInt( ctx.INT().getText() ));
   }
 
 	@Override 
   public void exitAddSub(ExprParser.AddSubContext ctx) { 
-    System.out.println( "Exiting AddSub" );
+    LOGGER.info( "Exiting AddSub" );
     int b = theStack.pop();
     int a = theStack.pop();
     int result;
@@ -48,13 +61,13 @@ public class EvalListener extends ExprBaseListener {
 
 	@Override 
   public void exitPrint(ExprParser.PrintContext ctx) { 
-    System.out.println( "Exiting Print" );
+    LOGGER.info( "Exiting Print" );
     System.out.println( theStack.pop());
   }
 
 	@Override 
   public void exitMulDiv(ExprParser.MulDivContext ctx) { 
-    System.out.println( "Exiting MulDiv" );
+    LOGGER.info( "Exiting MulDiv" );
     int b = theStack.pop();
     int a = theStack.pop();
     int result;
--- Expr.java
+++ Expr.java
@@ -10,12 +10,16 @@ public class Expr {
     if (args.length>0) inputFile = args[0];
     InputStream is = System.in;
     if (inputFile != null) is = new FileInputStream(inputFile);
+    String logLevel = "SEVERE";
+    if (args.length>1) logLevel = args[1];
     ANTLRInputStream inputStream = new ANTLRInputStream( is );
     ExprLexer lexer = new ExprLexer( inputStream );
     CommonTokenStream tokens = new CommonTokenStream( lexer );
     ExprParser parser = new ExprParser( tokens );
     ParseTree tree = parser.prog();
     ParseTreeWalker walker = new ParseTreeWalker();
-    walker.walk( new EvalListener(), tree );
+    EvalListener eval = new EvalListener();
+    if ( logLevel.equals("INFO") ) eval.setVerbose( true );
+    walker.walk( eval, tree );
   }
 }
