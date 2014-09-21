8101de3f9f8b39ce69fb0d12303c3bff6964f0b1
mattmcd <matt@matt-mcdonnell.com>
2014-09-21 14:09:21 +0100

    Trying to get assignment working correctly - still in progress. e.g a=b+1 does not work

--- EvalListener.java
+++ EvalListener.java
@@ -1,31 +1,38 @@
 import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
 
 public class EvalListener extends ExprBaseListener {
   private boolean isAssign;
   private Stack<Integer> theStack = new Stack<Integer>(); 
+  private Map<String,Integer> map = new HashMap<String,Integer>();
 
-	@Override public void enterId(ExprParser.IdContext ctx) { }
-	@Override public void exitId(ExprParser.IdContext ctx) { }
+	@Override public void exitId(ExprParser.IdContext ctx) { 
+    if (!isAssign) {
+      theStack.push( map.get( ctx.ID().getText()));
+    }
+  }
 
-	@Override public void enterAssign(ExprParser.AssignContext ctx) { isAssign = true; }
-	@Override public void exitAssign(ExprParser.AssignContext ctx) { isAssign = false; }
+	@Override public void enterAssign(ExprParser.AssignContext ctx) { 
+    isAssign = true; 
+  }
+	@Override public void exitAssign(ExprParser.AssignContext ctx) { 
+    map.put( ctx.ID().getText(), theStack.pop());
+    isAssign = false;
+  }
 
-	@Override public void enterProg(ExprParser.ProgContext ctx) { }
-	@Override public void exitProg(ExprParser.ProgContext ctx) { }
-
-	@Override public void exitUminus(ExprParser.UminusContext ctx) { 
+	@Override 
+  public void exitUminus(ExprParser.UminusContext ctx) { 
     theStack.push(-theStack.pop());
-    System.out.println( "Pushed " + theStack.peek());
   }
 
-	@Override public void exitInt(ExprParser.IntContext ctx) 
-  { 
+	@Override 
+  public void exitInt(ExprParser.IntContext ctx) { 
     theStack.push( Integer.parseInt( ctx.INT().getText() ));
-    System.out.println( "Pushed " + theStack.peek());
   }
 
-	@Override public void exitAddSub(ExprParser.AddSubContext ctx) 
-  { 
+	@Override 
+  public void exitAddSub(ExprParser.AddSubContext ctx) { 
     int b = theStack.pop();
     int a = theStack.pop();
     int result;
@@ -35,16 +42,15 @@ public class EvalListener extends ExprBaseListener {
       result = a-b;
     }
     theStack.push( result );
-    System.out.println( "Pushed " + theStack.peek());
   }
 
-	@Override public void exitPrint(ExprParser.PrintContext ctx) 
-  { 
+	@Override 
+  public void exitPrint(ExprParser.PrintContext ctx) { 
     System.out.println( theStack.pop());
   }
 
-	@Override public void exitMulDiv(ExprParser.MulDivContext ctx) 
-  { 
+	@Override 
+  public void exitMulDiv(ExprParser.MulDivContext ctx) { 
     int b = theStack.pop();
     int a = theStack.pop();
     int result;
@@ -54,6 +60,5 @@ public class EvalListener extends ExprBaseListener {
       result = a/b;
     }
     theStack.push( result );
-    System.out.println( "Pushed " + theStack.peek());
   }
 }
