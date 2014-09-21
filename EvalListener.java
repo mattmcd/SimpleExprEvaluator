847070a274eeece47590f363836aa559821885eb
mattmcd <matt@matt-mcdonnell.com>
2014-09-21 19:19:22 +0100

    Fix formatting.#

--- EvalListener.java
+++ EvalListener.java
@@ -8,7 +8,7 @@ public class EvalListener extends ExprBaseListener {
   private Stack<Integer> theStack = new Stack<Integer>(); 
   private Map<String,Integer> map = new HashMap<String,Integer>();
   private final static Logger LOGGER = Logger.getLogger(
-    EvalListener.class.getName());
+      EvalListener.class.getName());
 
   public EvalListener( ) {
     LOGGER.setLevel(Level.SEVERE);
@@ -19,64 +19,64 @@ public class EvalListener extends ExprBaseListener {
     else LOGGER.setLevel(Level.SEVERE);
   }
 
-	@Override public void exitId(ExprParser.IdContext ctx) { 
+  @Override public void exitId(ExprParser.IdContext ctx) { 
     LOGGER.info( "Exiting Id" );
     LOGGER.info( "  Pushing value of " + 
-      ctx.ID().getText() + " to stack" );
+        ctx.ID().getText() + " to stack" );
     theStack.push( map.get( ctx.ID().getText()));
   }
 
-	@Override public void enterAssign(ExprParser.AssignContext ctx) { 
+  @Override public void enterAssign(ExprParser.AssignContext ctx) { 
     LOGGER.info( "Entering Assign" );
   }
-	@Override public void exitAssign(ExprParser.AssignContext ctx) { 
+  @Override public void exitAssign(ExprParser.AssignContext ctx) { 
     LOGGER.info( "Exiting Assign" );
     map.put( ctx.ID().getText(), theStack.pop());
   }
 
-	@Override 
-  public void exitUminus(ExprParser.UminusContext ctx) { 
-    theStack.push(-theStack.pop());
-  }
-
-	@Override 
-  public void exitInt(ExprParser.IntContext ctx) { 
-    LOGGER.info( "Exiting Int" );
-    theStack.push( Integer.parseInt( ctx.INT().getText() ));
-  }
-
-	@Override 
-  public void exitAddSub(ExprParser.AddSubContext ctx) { 
-    LOGGER.info( "Exiting AddSub" );
-    int b = theStack.pop();
-    int a = theStack.pop();
-    int result;
-    if (ctx.op.getType() == ExprParser.ADD ) {
-      result = a+b;
-    } else {
-      result = a-b;
+  @Override 
+    public void exitUminus(ExprParser.UminusContext ctx) { 
+      theStack.push(-theStack.pop());
     }
-    theStack.push( result );
-  }
 
-	@Override 
-  public void exitPrint(ExprParser.PrintContext ctx) { 
-    LOGGER.info( "Exiting Print" );
-    System.out.println( theStack.pop());
-  }
-
-	@Override 
-  public void exitMulDiv(ExprParser.MulDivContext ctx) { 
-    LOGGER.info( "Exiting MulDiv" );
-    int b = theStack.pop();
-    int a = theStack.pop();
-    int result;
-    if (ctx.op.getType() == ExprParser.MUL) {
-      result = a*b;
-    } else {
-      result = a/b;
+  @Override 
+    public void exitInt(ExprParser.IntContext ctx) { 
+      LOGGER.info( "Exiting Int" );
+      theStack.push( Integer.parseInt( ctx.INT().getText() ));
     }
-    theStack.push( result );
-  }
-	
+
+  @Override 
+    public void exitAddSub(ExprParser.AddSubContext ctx) { 
+      LOGGER.info( "Exiting AddSub" );
+      int b = theStack.pop();
+      int a = theStack.pop();
+      int result;
+      if (ctx.op.getType() == ExprParser.ADD ) {
+        result = a+b;
+      } else {
+        result = a-b;
+      }
+      theStack.push( result );
+    }
+
+  @Override 
+    public void exitPrint(ExprParser.PrintContext ctx) { 
+      LOGGER.info( "Exiting Print" );
+      System.out.println( theStack.pop());
+    }
+
+  @Override 
+    public void exitMulDiv(ExprParser.MulDivContext ctx) { 
+      LOGGER.info( "Exiting MulDiv" );
+      int b = theStack.pop();
+      int a = theStack.pop();
+      int result;
+      if (ctx.op.getType() == ExprParser.MUL) {
+        result = a*b;
+      } else {
+        result = a/b;
+      }
+      theStack.push( result );
+    }
+
 }
